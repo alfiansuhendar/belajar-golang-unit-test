2d59a0c547b4d52e583f44db4f3014334f6a9fa2
alfian10 <alfian.suhendar@gmail.com>
2024-05-07 22:51:20 +0700

    unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,43 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Eko")
+	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Require Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Eko")
+	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
+
+func TestHelloWorldEko(t *testing.T) {
+	result := HelloWorld("Eko")
+
 	if result != "Hello Eko" {
-		// unit test failed
-		panic("Result is not Hello Eko")
+		// error
+		t.Error("Result must be 'Hello Eko'")
 	}
+
+	fmt.Println("TestHelloWorldEko Done")
+}
+
+func TestHelloWorldKhannedy(t *testing.T) {
+	result := HelloWorld("Khannedy")
+
+	if result != "Hello Khannedy" {
+		// error
+		t.Fatal("Result must be 'Hello Khannedy'")
+	}
+
+	fmt.Println("TestHelloWorldKhannedy Done")
 }
